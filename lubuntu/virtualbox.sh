c61a94e8d2ddc30b99d27489299edbead46d0215
Corey Ford <corey@coreyford.name>
2014-11-15 19:54:39 -0800

    Make shared folders work
    This gives the student user access to VirtualBox shared folders, and
    puts a shortcut to /media on the Desktop. Fixes #2.

--- lubuntu/virtualbox.sh
+++ lubuntu/virtualbox.sh
@@ -2,3 +2,9 @@
 set -e
 
 apt-get -qy install virtualbox-guest-x11
+
+# Improve shared folder usability
+rmdir /media/cdrom
+mkdir -p /home/student/Desktop
+ln -s /media /home/student/Desktop/shared
+usermod -aG vboxsf student
